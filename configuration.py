01dc65e8a53aef509b068f2f0a353b585fba893f
Pieter Belmans <pieterbelmans@gmail.com>
2019-07-15 16:50:19 +0200

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+# configuration for the website
+COMMENTS = "comments.sqlite"
+DATABASE = "hello-world.sqlite"
+UNIT = "section"
+DEPTH = 0
+
+# configuration for the import
+PATH = "document"
+PAUX = "document.paux"
+TAGS = "tags"
+PDF = ""
+
